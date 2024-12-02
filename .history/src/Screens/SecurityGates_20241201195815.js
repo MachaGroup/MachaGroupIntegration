ed370885570257e5b3ab4a06836d5f35cbf77969
ElizabethHall02 <77406513+ElizabethHall02@users.noreply.github.com>
2024-12-01 20:29:10 -0500

    Updated Navbar

--- .history/src/Screens/SecurityGates_20241201195815.js
+++ .history/src/Screens/SecurityGates_20241201195815.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getFirestore, collection, addDoc, doc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { useBuilding } from '../Context/BuildingContext'; // Context for buildingId
-import Navbar from "./Navbar"; // Import the Navbar
+import Navbar from "../components/Navbar"; // Import the Navbar
 import './FormQuestions.css';
 import logo from '../assets/MachaLogo.png'; // Adjust the path if necessary
 
