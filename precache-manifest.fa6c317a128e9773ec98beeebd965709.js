007f97d09caf560b0b4ab144a013e39489d04791
Macha Group <machagroupwebapp@gmail.com>
2025-01-24 16:55:19 -0500

    Updates

---
+++ precache-manifest.fa6c317a128e9773ec98beeebd965709.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9e5cb7c6551d490493b0ea6a6ba485",
+    "url": "/MachaGroupIntegration/index.html"
+  },
+  {
+    "revision": "3807fa23c4a21ef62079",
+    "url": "/MachaGroupIntegration/static/css/main.1ebe25ce.chunk.css"
+  },
+  {
+    "revision": "76e5d358b3921d4e0789",
+    "url": "/MachaGroupIntegration/static/js/2.9328999b.chunk.js"
+  },
+  {
+    "revision": "9c0c47d34de10fa07a7c",
+    "url": "/MachaGroupIntegration/static/js/3.cf3acbb1.chunk.js"
+  },
+  {
+    "revision": "3807fa23c4a21ef62079",
+    "url": "/MachaGroupIntegration/static/js/main.fa7cfa19.chunk.js"
+  },
+  {
+    "revision": "34b485568cd51bded309",
+    "url": "/MachaGroupIntegration/static/js/runtime~main.c7ef00b2.js"
+  },
+  {
+    "revision": "d57030893ac380bd1d3f8b6a66cb8e7d",
+    "url": "/MachaGroupIntegration/static/media/MachaLogo.d5703089.png"
+  }
+]);
